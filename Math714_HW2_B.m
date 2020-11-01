bca1df18ab39f4e007286a9bb01706c6342f2675
yangdy00 <72372272+yangdy00@users.noreply.github.com>
2020-10-31 23:07:22 -0500

    Update Math714_HW2_B.m

--- Math714_HW2_B.m
+++ Math714_HW2_B.m
@@ -5,6 +5,6 @@ while error > 10^-2
     x = 0:1/N:1;
     f = exp(-400*(x-0.5).^2);
     xq = 0:1/(1000*N):1;
-    error = max(interp1(x,f,xq,'linear')-exp(-400*(xq-0.5).^2));
+    error = max(abs(interp1(x,f,xq,'linear')-exp(-400*(xq-0.5).^2)));
 end
 disp(N)
