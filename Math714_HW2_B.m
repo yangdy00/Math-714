f001a914af85ee8ae83eeaec6829109a0d0c4d22
yangdy00 <72372272+yangdy00@users.noreply.github.com>
2020-10-31 22:49:41 -0500

    Add files via upload

---
+++ Math714_HW2_B.m
@@ -0,0 +1,10 @@
+error = 1;
+N = 10;
+while error > 10^-2
+    N = N+1;
+    x = 0:1/N:1;
+    f = exp(-400*(x-0.5).^2);
+    xq = 0:1/(1000*N):1;
+    error = max(interp1(x,f,xq,'linear')-exp(-400*(xq-0.5).^2));
+end
+disp(N)
